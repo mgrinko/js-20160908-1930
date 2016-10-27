c0cb592745a6a2a729e16a1366eb74de26df92b2
mgrinko <glukym@gmail.com>
2016-10-27 21:07:17 +0300

    Filter was added

--- scripts/item-catalogue.js
+++ scripts/item-catalogue.js
@@ -11,7 +11,13 @@ class ItemCatalogue extends Component {
 
     this._render(this._items);
 
+    this._filter = new Filter({
+      element: this._el.querySelector('[data-component="filter"]')
+    });
+
+
     this._el.addEventListener('click', this._onItemDetailsLinkClick.bind(this));
+    this._filter.getElement().addEventListener('filter.change', this._onFilterChange.bind(this));
   }
 
   _render(items) {
@@ -34,6 +40,20 @@ class ItemCatalogue extends Component {
     }
 
     this._triggerItemSelectedEvent(itemContainer.dataset.itemId);
+  }
+
+  _onFilterChange(event) {
+    let filterValue = event.detail;
+    let itemsToRender = this._getItems(filterValue);
+
+    this._render(itemsToRender)
+  }
+
+  _getItems(query) {
+    return this._items.filter(function(item) {
+      return (item.name.indexOf(query) !== -1)
+        || (item.snippet.indexOf(query) !== -1);
+    });
   }
 
   _triggerItemSelectedEvent(itemId) {
